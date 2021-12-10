362e12b1f7354f0befe217f8e7ce243145123ada
wawa1919 <wawa1919@colorado.edu>
2021-12-09 18:03:57 -0700

    add files

--- server.js
+++ server.js
@@ -1,17 +1,12 @@
 var express = require('express'); //Ensure our express framework has been added
-var session = require('express-session');
+//var session = require('express-session');
+var axios = require('axios');
 var app = express();
 var bodyParser = require('body-parser'); //Ensure our body-parser tool has been added
-app.use(session({secret: 'ssshhhhh',saveUninitialized: true,resave: true}));
+//app.use(session({secret: 'ssshhhhh',saveUninitialized: true,resave: true}));
 app.use(bodyParser.json());              // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
-// set the view engine to ejs
-//app.set('views', '/home/node/app/views');
-//app.engine('html', require('ejs').renderFile)
-//app.set('view engine', 'ejs');
-//app.use(express.static('resources/styles'));
-//app.use(express.static(__dirname + {index: 'index.ejs'}));
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/'));// Set the relative path; makes accessing the resource directory easier
 
@@ -25,10 +20,26 @@ app.get('/reviews', function(req, res){
 
 app.post('/get_feed', function(req, res) {
 	var tvshow = req.body.searchBar; //TODO: Remove null and fetch the param (e.g, req.body.param_name); Check the NYTimes_home.ejs file or console.log("request parameters: ", req) to determine the parameter names
-  
-	console.log(`https://www.tvmaze.com/search/shows?q=${tvshow}`);
+	console.log(`https://api.tvmaze.com/search/shows?q=${tvshow}`);
+	//res.redirect(`https://www.tvmaze.com/search/shows?q=${tvshow}`);
 	if(tvshow) {
-	  axios({
+		axios({
+			url: `https://api.tvmaze.com/search/shows?q=${tvshow}`,
+			  method: 'GET',
+			  dataType:'json',
+			})
+			  .then(res => {
+				// TODO: Return the reviews to the front-end (e.g., res.render(...);); Try printing 'items' to the console to see what the GET request to the Twitter API returned.
+				// Did console.log(items) return anything useful? How about console.log(items.data.results)?
+				// Stuck? Look at the '/' route above
+				console.log(res.data);
+			  })
+			  .catch(error => {
+				console.log(error);
+				
+			  }); 
+
+	  /*axios({
 		url: `https://www.tvmaze.com/search/shows?q=${tvshow}`,
 		  method: 'GET',
 		  dataType:'json',
@@ -40,7 +51,8 @@ app.post('/get_feed', function(req, res) {
 			console.log(items.data.results);
 			res.render('pages/main',{
 			my_title: "TV Show Results",
-			//items: items.data,
+			heading: items.data.results[0].name,
+			items: items.data,
 			error: false,
 			message: ''
 			})
@@ -49,11 +61,12 @@ app.post('/get_feed', function(req, res) {
 			console.log(error);
 			res.render('pages/main', {
 			my_title: "TV Show Results",
-			//items: items.data,
+			items: '',
 			error: true,
 			message: error
 			})
-		  });
+			
+		  }); */
   
   
 	}
@@ -62,12 +75,17 @@ app.post('/get_feed', function(req, res) {
 	  // Stuck? On the web page, try submitting a search query without a search term
 	  res.render('pages/main', {
 		my_title: "TV Show Results",
-		//items: items.data,
+		items: '',
 		error: true,
 		message: 'No tv shows found!'
 	  })
 	}
+	
   });  
+
+app.get('/showsdisplay', function(req, res){
+	res.render('showsdisplay.ejs');
+});
 
 app.listen(3000);
 console.log('3000 is the magic port');
