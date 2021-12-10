902e9106d73a91800bbc72915c80cc410437b521
wawa1919 <wawa1919@colorado.edu>
2021-12-09 23:14:43 -0700

    add functioning search bar

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 var express = require('express'); //Ensure our express framework has been added
-//var session = require('express-session');
 var axios = require('axios');
 var app = express();
 var bodyParser = require('body-parser'); //Ensure our body-parser tool has been added
@@ -7,8 +6,33 @@ var bodyParser = require('body-parser'); //Ensure our body-parser tool has been 
 app.use(bodyParser.json());              // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
+//Create Database Connection
+var pgp = require('pg-promise')();
+
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/'));// Set the relative path; makes accessing the resource directory easier
+
+const dev_dbConfig = {
+	host: 'db',
+	port: 5432,
+	database: process.env.POSTGRES_DB,
+	user:  process.env.POSTGRES_USER,
+	password: process.env.POSTGRES_PASSWORD
+};
+
+/** If we're running in production mode (on heroku), the we use DATABASE_URL
+ * to connect to Heroku Postgres.
+ */
+const isProduction = process.env.NODE_ENV === 'production';
+const dbConfig = isProduction ? process.env.DATABASE_URL : dev_dbConfig;
+
+// Heroku Postgres patch for v10
+// fixes: https://github.com/vitaly-t/pg-promise/issues/711
+if (isProduction) {
+  pgp.pg.defaults.ssl = {rejectUnauthorized: false};
+}
+
+const db = pgp(dbConfig);
 
 app.get('/', function(req, res){
     res.render('index.ejs');
@@ -28,46 +52,30 @@ app.post('/get_feed', function(req, res) {
 			  method: 'GET',
 			  dataType:'json',
 			})
-			  .then(res => {
+			  .then(response => {
 				// TODO: Return the reviews to the front-end (e.g., res.render(...);); Try printing 'items' to the console to see what the GET request to the Twitter API returned.
 				// Did console.log(items) return anything useful? How about console.log(items.data.results)?
 				// Stuck? Look at the '/' route above
-				console.log(res.data);
+				console.log(response.data);
+				console.log(response.data[0].show.name);
+				console.log(response.data[0].show.genres);
+				console.log(response.data[0].show.summary);
+				console.log(response.data[0].show.rating);
+				console.log(response.data[0].show.image.medium);
+				//const obj = JSON.parse(res.data);
+				//console.log(obj);
+				res.render('pages/showsdisplay',{
+					name: response.data[0].show.name,
+					genres: response.data[0].show.genres,
+					summary: response.data[0].show.summary,
+					ratings: response.data[0].show.rating.average,
+					image: response.data[0].show.image.medium
+				})
+
 			  })
 			  .catch(error => {
-				console.log(error);
-				
-			  }); 
-
-	  /*axios({
-		url: `https://www.tvmaze.com/search/shows?q=${tvshow}`,
-		  method: 'GET',
-		  dataType:'json',
-		})
-		  .then(items => {
-			// TODO: Return the reviews to the front-end (e.g., res.render(...);); Try printing 'items' to the console to see what the GET request to the Twitter API returned.
-			// Did console.log(items) return anything useful? How about console.log(items.data.results)?
-			// Stuck? Look at the '/' route above
-			console.log(items.data.results);
-			res.render('pages/main',{
-			my_title: "TV Show Results",
-			heading: items.data.results[0].name,
-			items: items.data,
-			error: false,
-			message: ''
-			})
-		  })
-		  .catch(error => {
-			console.log(error);
-			res.render('pages/main', {
-			my_title: "TV Show Results",
-			items: '',
-			error: true,
-			message: error
-			})
-			
-		  }); */
-  
+				console.log(error);	
+			  })
   
 	}
 	else {
@@ -87,5 +95,63 @@ app.get('/showsdisplay', function(req, res){
 	res.render('showsdisplay.ejs');
 });
 
-app.listen(3000);
-console.log('3000 is the magic port');
+app.post('/addreview', function(req, res) {
+	var showId = req.body.show_label;
+	var showTitle = req.body.show_label;
+	var showReview = req.body.review_label;
+	var showDate = req.body.show_label;
+	var insert_into_table = "INSERT INTO reviews(id, tv_show, review, review_date) VALUES('" + 1 + "','" + showTitle  + "','" + showTitle + "','" + '18760401'+ "');";
+	db.task('get-everything', task => {
+        return task.batch([
+            task.any(insert_into_table), // info[0]
+        ]);
+    })
+    .then(info => {
+        res.render('reviews.ejs',{
+                my_title: "Review page",
+				/*
+                First_name: info[1][0].first_name,
+                Last_name: info[1][0].last_name,
+                Username: info[1][0].username,
+                Email: info[1][0].email,
+                Password: info[1][0].password,
+				*/
+            })
+    })
+    .catch(err => {
+            console.log('error', err);
+            res.render('reviews.ejs', {
+                my_title: "Review page",
+                first_name: '',
+                Last_name: '',
+                Username: '',
+                Email: '',
+                Password: '',
+            })
+    });
+});
+
+app.get('/reviews', function(req, res) {
+	var query = 'SELECT * FROM reviews;';
+	db.any(query)
+		.then(function (rows) {
+			console.log(data);
+			res.render('reviews.ejs',{
+				my_title: "Reviews Table",
+				reviewData: data
+			})
+
+		})
+		.catch(function (err) {
+			// display error message in case an error
+			console.log('error', err);
+			res.render('reviews.ejs',{
+				my_title: "Reviews Table",
+				data: ''
+			})
+		})
+});
+
+const server = app.listen(process.env.PORT || 3000, () => {
+	console.log(`Express running → PORT ${server.address().port}`);
+  });
